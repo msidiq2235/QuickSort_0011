2cfe48389c5ceb017da6a0b945229e6a315827cc
msidiq2235 <muhsidiq2005@gmail.com>
2024-03-28 18:42:46 +0700

    10

--- Algoritma2803/Algoritma2803.cpp
+++ Algoritma2803/Algoritma2803.cpp
@@ -58,6 +58,12 @@ void q_short(int low, int high)
 		}
 		cmp_count++;
 		//search for an element Less than or equal to pivot
-		while ((aee[j] > pivot) && (j >= low)) //step 7
+		while ((arr[j] > pivot) && (j >= low)) //step 7
+		{
+			j--; //step 8
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j) //step 9
 	}
 }
